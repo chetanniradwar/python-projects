f8873d1611cb15aeff173bc0713c123467840a60
chetanniradwar <chetan@kafqa.com>
2022-11-29 09:10:15 +0530

    black and white spirograph

--- spirograph_with_turtle.py
+++ spirograph_with_turtle.py
@@ -1,1 +1,30 @@
+import turtle
 
+t = turtle.Turtle()
+
+t.speed(0)
+
+def draw_circle(radius):
+    # move
+    t.penup()
+    t.right(90)
+    t.forward(radius)
+    t.left(90)
+    t.pendown()
+
+    # circle
+    t.circle(radius)
+
+    # move back
+    t.penup()
+    t.right(-90)
+    t.forward(radius)
+    t.left(-90)
+    t.pendown()
+
+for _ in range(int(360/5)):
+    t.penup()
+    t.circle(100, 5)
+    draw_circle(100)
+
+turtle.exitonclick()
